76370d000a58f9b7ad14dcb119009af239e048c8
Maxim Smirnov <m.smirnov@latera.ru>
2019-04-24 17:47:50 +0300

    HBW-137 Add duration of requests logging

--- hbw/app/controllers/hbw/base_controller.rb
+++ hbw/app/controllers/hbw/base_controller.rb
@@ -2,11 +2,23 @@ module HBW
   class BaseController < ActionController::Base
     include Controller
     include HttpBasicAuthentication
+    before_action :start
+    after_action :log
 
     protected
 
     def current_user_identifier
       user_identifier || current_user.email
+    end
+
+    private
+
+    def start
+      @start = Time.now.to_f
+    end
+
+    def log
+      logger.tagged('DURATION') { logger.info { "#{(Time.now.to_f - @start) * 1000.0}ms, #{request.method} #{request.original_fullpath}" } }
     end
   end
 end
